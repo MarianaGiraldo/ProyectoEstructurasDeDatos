dfbabfc49628693f0807ecc935ce5768c82a8aa1
11ZaidaG11 <zguzmanm@unal.edu.co>
2025-12-01 14:42:44 -0500

    Logo UNAL

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,22 +5,25 @@ import { SearchBar } from "../components/SearchBar";
 function Home() {
   return (
     <div className="Home">
+
       <div className="title_cont">
         <div>PLANES DE ESTUDIO</div>
       </div>
+
       <div className="search_bar_cont">
         <SearchBar />
         <div>SearchResults</div>
       </div>
-      <div>Bienvenido, en esta pagina 
-      podrás saber cual es la mínima 
-      cantidad de semestres que te 
-      puedes demorar en completar tu 
-      carrera profesional en UNAL, 
-      para comenzar selecciona tu 
-      plan de estudios, si no lo 
-      encuentras lo puedes ingresar 
-      manualmente.</div>
+      
+      <div className="welcome">
+        <img src="unal_logo.png" alt="UNAL Logo" className="unal_logo" />
+        <div>Bienvenido, en esta pagina podrás saber cual es la mínima 
+        cantidad de semestres que te puedes demorar en completar tu 
+        carrera profesional en UNAL, para comenzar selecciona tu 
+        plan de estudios, si no lo encuentras lo puedes ingresar 
+        manualmente.</div>
+      </div>
+
     </div>
   );
 }
