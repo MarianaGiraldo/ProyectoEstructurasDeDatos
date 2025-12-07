6f34e2744a8250b2caebedc413ba4d390513908a
11ZaidaG11 <zguzmanm@unal.edu.co>
2025-12-07 15:48:25 -0500

    Avance planes de estudio - Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,25 +7,33 @@ import { TbBackground } from "react-icons/tb";
 function Home() {
   return (
     <div className="Home">
-      <div style = {{backgroundImage: `url(${unlogo})`, width: "30px", height: "30px"}}>
+
+      <div className="horizontal">
+        <div className="title_cont">
+          <div>PLANES DE ESTUDIO</div>
+        </div>
+
+        <div className="search_bar_cont">
+          <SearchBar />
+          <div>SearchResults</div>
+        </div>
       </div>
 
-      <div className="title_cont">
-        <div>PLANES DE ESTUDIO</div>
-      </div>
+      <div className="vert_cont">
 
-      <div className="search_bar_cont">
-        <SearchBar />
-        <div>SearchResults</div>
-      </div>
-      
-      <div className="welcome">
-        <img src="unal_logo.png" alt="UNAL Logo" className="unal_logo" />
-        <div>Bienvenido, en esta pagina podrás saber cual es la mínima 
-        cantidad de semestres que te puedes demorar en completar tu 
-        carrera profesional en UNAL, para comenzar selecciona tu 
-        plan de estudios, si no lo encuentras lo puedes ingresar 
-        manualmente.</div>
+        <div className="vert_cont left">
+          Planes de estudio disponibles
+        </div>
+
+        <div className="vert_cont right">
+          <p>
+          Bienvenido, en esta pagina podrás saber cual es la mínima 
+          cantidad de semestres que te puedes demorar en completar tu 
+          carrera profesional en UNAL, para comenzar selecciona tu 
+          plan de estudios, si no lo encuentras lo puedes ingresar 
+          manualmente.
+          </p>
+        </div>
       </div>
 
     </div>
