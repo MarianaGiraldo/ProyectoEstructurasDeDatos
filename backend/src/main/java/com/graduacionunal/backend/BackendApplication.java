ff91948447ff0c841397111fadc789eee8806905
Mariana Giraldo <magavi.gilu@gmail.com>
2025-12-09 09:55:43 -0500

    Registers modesl and repositories to BackendApplication

--- backend/src/main/java/com/graduacionunal/backend/BackendApplication.java
+++ backend/src/main/java/com/graduacionunal/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package com.graduacionunal.backend;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.graduacionunal.backend")
+@EntityScan(basePackages = "com.graduacionunal.backend.models")
+@EnableJpaRepositories(basePackages = "com.graduacionunal.backend.repositories")
 public class BackendApplication {
 
 	public static void main(String[] args) {
